3ee90cfc9925e67fd9ef4655aa49cde3f13bf145
jhhsu8 <jhhsu11@gmail.com>
2020-03-21 23:01:12 -0700

    Update basic_plots.R

--- basic_plots.R
+++ basic_plots.R
@@ -1,32 +1,26 @@
 # Basic Plots
 
-#1. Scatterplots 
-
 # Load the datasets and define some variables
 library(dslabs)
 data(murders)
+
+#1. Scatterplots 
 population_in_millions <- murders$population/10^6
 total_gun_murders <- murders$total
 plot(population_in_millions, total_gun_murders)
-
 # Transform population using the log10 transformation and save to object log10_population
 log10_population <- log10(murders$population)
-
 # Transform total gun murders using log10 transformation and save to object log10_total_gun_murders
 log10_total_gun_murders <- log10(murders$total)
-
 # Create a scatterplot with the log scale transformed population and murders 
 plot(log10_population, log10_total_gun_murders)
 
 #2. Histograms 
-
 # Store the population in millions and save to population_in_millions 
 population_in_millions <- murders$population/10^6
-
 # Create a histogram of this variable
 hist(population_in_millions)
 
 #3. Boxplots 
-
 # Create a boxplot of state populations by region for the murders dataset
 boxplot(population ~ region, data = murders)
